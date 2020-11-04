d5747181e1296a2f4a10bcbeb4ef3b0e35d5163a
Asur-harsh <73747616+Asur-harsh@users.noreply.github.com>
2020-11-04 14:20:54 +0530

    Computing op2

--- arithmeticOperations.sh
+++ arithmeticOperations.sh
@@ -9,3 +9,6 @@ read -p "Enter third input :" thirdInput
 #COMPUTE ARITHMETIC OPERATION
 result=`echo "scale=2;$firstInput + $secondInput * $thirdInput" | bc`
 echo "Result:" $result
+#op2
+resultOne=`echo "scale=2;$firstInput * $secondInput + $thirdInput" | bc`
+echo "Result:" $resultOne
