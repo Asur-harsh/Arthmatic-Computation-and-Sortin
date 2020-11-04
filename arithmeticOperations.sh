c976014cd85a255a9d77901f0954ee3c20ef634d
Asur-harsh <73747616+Asur-harsh@users.noreply.github.com>
2020-11-04 14:05:28 +0530

    Added three inputs

---
+++ arithmeticOperations.sh
@@ -0,0 +1,7 @@
+#!/bin/bash/
+echo "--------------------------------Arithmetic Compution And Sorting ----------------------------------"
+
+#TO TAKING INPUT FROM USER
+read -p "Enter first input :" firstInput
+read -p "Enter second input:" secondInput
+read -p "Enter third input :" thirdInput
